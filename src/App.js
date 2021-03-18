03e37ae5ecb862c0d4608b15b88c96537749f951
hithesh98 <kirrkirry@gmail.com>
2021-03-18 13:30:42 +0000

    add input box and particles background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className = 'particles'
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm/>
-      <FaceRecognition /> */}
+      <Rank />
+      <ImageLinkForm/>
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
