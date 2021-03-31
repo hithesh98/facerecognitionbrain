72f7f4580f711e553c021af9b006618f7efe480e
hithesh98 <kirrkirry@gmail.com>
2021-03-31 20:55:37 +0100

    connect front end to back end locally

--- src/App.js
+++ src/App.js
@@ -37,6 +37,13 @@ class App extends Component {
       isSignedIn: false
     }
   }
+
+  componentDidMount(){
+    fetch('http://localhost:3001')
+      .then(response => response.json())
+      .then(console.log)
+  }
+
   calculateFaceLocation =(data)=>{
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
