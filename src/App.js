7db12a7d2fa17e1746f91bdd4aaf5800f351fc40
hithesh98 <kirrkirry@gmail.com>
2021-04-01 13:47:07 +0100

    add basic register

--- src/App.js
+++ src/App.js
@@ -34,10 +34,28 @@ class App extends Component {
       imageURL:'',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
+  loadUser = (data) => { 
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined 
+      }
+    })
+
+  }
   componentDidMount(){
     fetch('http://localhost:3001')
       .then(response => response.json())
@@ -98,7 +116,7 @@ class App extends Component {
          : (
            route === 'signin'
             ? <SignIn onRouteChange={this.onRouteChange} />
-            : <Register onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
          ) 
         }
       </div>
